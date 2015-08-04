56c84cc2f619e4ebfde24794cd0bb854a2f47855
Nazar Aziz <mcnazar@gmail.com>
2015-08-04 13:15:30 +0100

    fixes #21

--- src/app/utils/roundFormated.js
+++ src/app/utils/roundFormated.js
@@ -1,6 +1,13 @@
 import _ from 'lodash';
 
 export default function roundFormatted( weight, places = 0 ) {
-    let rounded = _.round( weight, places );
-    return parseFloat( rounded ).toLocaleString();
+    let rounded;
+
+    if ( weight ) {
+        rounded = _.round( weight, places );
+        return parseFloat( rounded ).toLocaleString();
+    } else {
+        return 0;
+    }
+
 }
