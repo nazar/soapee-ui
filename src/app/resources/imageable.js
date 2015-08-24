20098eeef60d905a41a29482de9a9ea6dced0f2b
Nazar Aziz <mcnazar@gmail.com>
2015-08-24 14:12:14 +0100

    refs #41 - recipe edit add/remove images

--- src/app/resources/imageable.js
+++ src/app/resources/imageable.js
@@ -6,3 +6,7 @@ import config from 'config';
 export function imageableUrl( image ) {
     return resolve( config.imageUrl, [ image.path, image.file_name ].join( '/' ) );
 }
+
+export function imageableThumbUrl( image ) {
+    return resolve( config.imageUrl, [ image.path, 'thumb-' + image.file_name ].join( '/' ) );
+}
