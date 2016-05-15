bbc0dbda6005efaa2b32a8142739a11644e90853
Nazar Aziz <mcnazar@gmail.com>
2016-05-15 18:28:04 +0100

    cleanup share buttons

--- src/app/components/shareThis.js
+++ src/app/components/shareThis.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import config from 'config';
 
-import { TwitterButton, TwitterCount } from 'react-social';
+import { TwitterButton, PinterestButton, RedditButton, TwitterCount, PinterestCount, RedditCount } from 'react-social';
 
 import ButtonFBLike from 'components/buttonFBLike';
 import ButtonGPlusLike from 'components/buttonGPlusLike';
@@ -16,8 +16,22 @@ export default React.createClass( {
                     <TwitterButton
                         className="btn btn-primary btn-xs hidden-sm"
                         url={ url }>
-                        <i className="fa fa-twitter"> &nbsp;&nbsp;<TwitterCount url={ url } /></i>
+                        <i className="fa fa-twitter"></i>
                     </TwitterButton>
+                </div>
+                <div className="t-cell">
+                    <RedditButton
+                        className="btn btn-primary btn-reddit btn-xs hidden-sm"
+                        url={ url }>
+                        <i className="fa fa-pinterest"> &nbsp;&nbsp;<RedditCount url={url}/></i>
+                    </RedditButton>
+                </div>
+                <div className="t-cell">
+                    <PinterestButton
+                        className="btn btn-primary btn-pinterest btn-xs hidden-sm"
+                        url={ url }>
+                        <i className="fa fa-pinterest"> &nbsp;&nbsp;<PinterestCount url={url}/></i>
+                    </PinterestButton>
                 </div>
                 <div className="t-cell">
                     <ButtonGPlusLike
